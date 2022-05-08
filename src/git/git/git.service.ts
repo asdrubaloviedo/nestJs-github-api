95d797f2f3b2b504ad2c5a44ab045d38a606f054
asdrubal <asdrubal@mediabyte.com.pe>
2022-05-07 23:18:29 -0500

    getCommitsInformation api configuration

--- src/git/git/git.service.ts
+++ src/git/git/git.service.ts
@@ -11,30 +11,26 @@ export class GitService {
     return this.httpService
       .get(`https://api.github.com/users/${params.username}`)
       .pipe(
-        map((response) => response.data),
-        map((data) => ({
-          login: data.login,
-          url: data.url,
-          public_repos: data.public_repos,
-          created_at: data.created_at,
-          updated_at: data.updated_at,
+        map((response) => ({
+          login: response.data.login,
+          url: response.data.url,
+          public_repos: response.data.public_repos,
+          created_at: response.data.created_at,
+          updated_at: response.data.updated_at,
         })),
       );
   }
 
   getCommitsInformation(params) {
-    console.log('params :>> ', params);
+    // console.log('params :>> ', params);
 
     return this.httpService
       .get(`https://api.github.com/repos/${params.username}/${params.repository}/commits`)
       .pipe(
-        map((response, index) => {
-          console.log('index :>> ', index);
-          console.log('response.data :>> ', response.data[0].commit.author);
-          response.data[0].commit.author;
-        }),
-        map((data) => ({
-          data: data,
+        map((response) => ({
+          author: response.data[0].commit.author,
+          message: response.data[0].commit.message,
+          url: response.data[0].commit.url,
         })),
       );
   }
